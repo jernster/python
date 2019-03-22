3afca1b59fbe067fb4e44c6ca5f1ff7efa87e9a5
Jon Ernster <jon.ernster@gmail.com>
2019-03-21 18:54:37 -0600

    WIP: New Features added:
    * Added argparse functionality
    * Organized into functions
    * Made more pythonic

--- stock_data/stocks.py
+++ stock_data/stocks.py
@@ -3,44 +3,126 @@ import os
 import sys
 from alpha_vantage.timeseries import TimeSeries
 
-# PVTL, CLGX, GOOG, AMZN, NFLX, APPL, IBM, GE, CAT, AA, FB, YHOO, HPE
+# PVTL CLGX, GOOG AMZN NFLX APPL IBM GE CAT AA FB YHOO HPE
 
 parser = argparse.ArgumentParser(description="Generate various flavors of stock data")
 parser.add_argument('-s', '--symbol', nargs='+', help="stock symbol", required=True) 
-parser.add_argument('-i', '--interval', help="stock data interval", required=True) #intraday, weekly, weekly adjusted, monthly, monthly adjusted, batch stock
+parser.add_argument('-t', '--type', choices=['batch', 'intraday', 'weekly', 'weekly_adjusted',
+                                            'monthly', 'monthly_adjusted'], help="stock data type", required=True)
 args = parser.parse_args()
 
 symbol = args.symbol
-interval = args.interval
+stock_type = args.type
 
 ts = TimeSeries(key=os.environ['AV_API_KEY'], output_format='csv')
 
-# data, _ = ts.get_intraday(
-#    symbol=sys.argv[1], interval='5min', outputsize='full') #outputsize='compact' -- what's the diff? # intraday # daily #daily adjusted
-
-# data, _ = ts.get_weekly(symbol=sys.argv[1]) # get_weekly_adjusted
-# data, _ = ts.get_weekly_adjusted(symbol=sys.argv[1]) # get_weekly_adjusted
-# data, _ = ts.get_monthly(symbol=sys.argv[1]) # get_monthly_adjusted
-# data, _ = ts.get_monthly_adjusted(symbol=sys.argv[1]) # get_monthly_adjusted
-
-if interval == 'get_batch_stock_quotes':
-    #data, _ = ts.get_batch_stock_quotes(symbols=['PVTL', 'CLGX', 'GOOG', 'AMZN', 'NFLX',
-    #                                         'APPL', 'IBM', 'GE', 'CAT', 'AA', 'FB', 'YHOO', 'HPE'])
+def batch(symbol):
     data, _ = ts.get_batch_stock_quotes(symbols=symbol)
 
-# for everything except ts.get_batch_stock_quotes
-# for i, v in enumerate(data):
-#     if i == 0:
-#         s = 'symbol'
-#         v.insert(0, s)
-#         v = ",".join(v)
-#         print(v)
-#     elif i > 0:
-#         symbol = '"{}"'.format(sys.argv[1])
-#         v[0] = '"{}"'.format(v[0])
-#         v.insert(0, symbol)
-#         v = ",".join(v)
-#         print(v)
+    for v in (data):
+        print(v)
+
+def intraday(symbol):
+    data, _ = ts.get_intraday(
+    symbol=symbol, interval='1min', outputsize='compact') #outputsize='compact' and full 
 
     for i, v in enumerate(data):
-        print(i, v)
+        if i == 0:
+            s = 'symbol'
+            v.insert(0, s)
+            v = ",".join(v)
+            print(v)
+        elif i > 0:
+            s = ''.join(symbol) 
+            symbol = '{}'.format(s)
+            v[0] = '"{}"'.format(v[0])
+            v[0] = '{}'.format(v[0])
+            v.insert(0, symbol)
+            v = ",".join(v)
+            print(v)
+
+def weekly(symbol):
+    data, _ = ts.get_weekly(symbol=symbol) 
+
+    for i, v in enumerate(data):
+        if i == 0:
+            s = 'symbol'
+            v.insert(0, s)
+            v = ",".join(v)
+            print(v)
+        elif i > 0:
+            s = ''.join(symbol) 
+            symbol = '{}'.format(s)
+            v[0] = '"{}"'.format(v[0])
+            v[0] = '{}'.format(v[0])
+            v.insert(0, symbol)
+            v = ",".join(v)
+            print(v)
+
+def weekly_adjusted(symbol):
+    data, _ = ts.get_weekly_adjusted(symbol=symbol) 
+
+    for i, v in enumerate(data):
+        if i == 0:
+            s = 'symbol'
+            v.insert(0, s)
+            v = ",".join(v)
+            print(v)
+        elif i > 0:
+            s = ''.join(symbol) 
+            symbol = '{}'.format(s)
+            v[0] = '"{}"'.format(v[0])
+            v[0] = '{}'.format(v[0])
+            v.insert(0, symbol)
+            v = ",".join(v)
+            print(v)
+
+def monthly(symbol):
+    data, _ = ts.get_monthly(symbol=symbol) 
+
+    for i, v in enumerate(data):
+        if i == 0:
+            s = 'symbol'
+            v.insert(0, s)
+            v = ",".join(v)
+            print(v)
+        elif i > 0:
+            s = ''.join(symbol) 
+            symbol = '{}'.format(s)
+            v[0] = '"{}"'.format(v[0])
+            v[0] = '{}'.format(v[0])
+            v.insert(0, symbol)
+            v = ",".join(v)
+            print(v)
+
+def monthly_adjusted(symbol):
+    data, _ = ts.get_monthly_adjusted(symbol=symbol) 
+
+    for i, v in enumerate(data):
+        if i == 0:
+            s = 'symbol'
+            v.insert(0, s)
+            v = ",".join(v)
+            print(v)
+        elif i > 0:
+            s = ''.join(symbol) 
+            symbol = '{}'.format(s)
+            v[0] = '"{}"'.format(v[0])
+            v[0] = '{}'.format(v[0])
+            v.insert(0, symbol)
+            v = ",".join(v)
+            print(v)
+
+if __name__ == "__main__":
+    if stock_type == 'batch':
+        batch(symbol)
+    elif stock_type == 'intraday':
+        intraday(symbol)
+    elif stock_type == 'weekly':
+        weekly(symbol)
+    elif stock_type == 'weekly_adjusted':
+        weekly_adjusted(symbol)
+    elif stock_type == 'monthly':
+        monthly(symbol)
+    elif stock_type == 'monthly_adjusted':
+        monthly_adjusted(symbol)
